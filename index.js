2a54321a7d2da8ec28836a0e214b2af75700e2fa
alugtu <angelo42497@yahoo.com>
2021-10-08 01:33:37 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let clicker;
+currentAction = 'stop';
+count = 0;
+
+
+function onStartClick() {
+    document.getElementById('status').innerHTML = "Started";
+    // Prevent double instance of setInterval
+    if(currentAction !== 'start') {
+        currentAction = 'start';
+        clicker = setInterval(doClick, 1000);
+    }
+}
+
+function onStopClick() {
+    document.getElementById('status').innerHTML = "Stopped";
+    document.getElementById('countText').innerHTML = `Retriggers: 0`;
+    currentAction = 'stop';
+    clearInterval(clicker);
+}
+  
+function doClick() {
+    document.getElementById('trigger').click();
+}
+
+function addCount() {
+    document.getElementById('countText').innerHTML = `Retriggers: ${count + 1}`;
+    count += 1;
+}
